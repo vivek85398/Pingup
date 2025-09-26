7b693fac7faea1694ee3379019682aed564c630b
vivek85398 <your-github-email@example.com>
2025-09-26 15:20:03 +0530

    server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 await connectDB();
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('server is running');
@@ -21,4 +21,4 @@ const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
