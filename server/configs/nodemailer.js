40724ee507092da0c7e404713122027873e3cb5c
vivek85398 <your-github-email@example.com>
2025-10-16 02:33:09 +0530

    final commit

--- server/configs/nodemailer.js
+++ server/configs/nodemailer.js
@@ -5,19 +5,19 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: process.env.SMPT_USER,
+    user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
 
-const sendEmail = async ({to, subject, body}) => {
-    const response = await transporter.sendMail({
-        from: process.env.SENDER_EMAIL,
-        to,
-        subject,
-        html: body
-    });
-    return response;
-}
+const sendEmail = async ({ to, subject, body }) => {
+  const response = await transporter.sendMail({
+    from: process.env.SENDER_EMAIL,
+    to,
+    subject,
+    html: body,
+  });
+  return response;
+};
 
 export default sendEmail;
